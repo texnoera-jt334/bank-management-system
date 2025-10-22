f4e0ec4504e8eec0822dd0d3ee9eb8d141c2d993
Ali Hüseyn <ali.hsynov@gmail.com>
2025-10-23 02:19:16 +0400

    Initial commit - Spring Boot project setup

---
+++ src/test/java/az/banking/bankmanagementsystem/BankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package az.banking.bankmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
