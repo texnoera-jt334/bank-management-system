f55daec7b01a20ca03942ff79763b4f2ae3b7e41
Ali Hüseyn <ali.hsynov@gmail.com>
2025-10-27 19:40:37 +0400

    Create CustomerService interface for customer operations

---
+++ src/main/java/az/banking/bankmanagementsystem/service/CustomerService.java
@@ -0,0 +1,17 @@
+package az.banking.bankmanagementsystem.service;
+
+import az.banking.bankmanagementsystem.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    Customer createCustomer(Customer customer);
+
+    Customer getCustomerByFinCode(String finCode);
+
+    List<Customer> getAllCustomers();
+
+    Customer updateCustomer(String finCode, Customer customer);
+
+    void deleteCustomer(String finCode);
+}
