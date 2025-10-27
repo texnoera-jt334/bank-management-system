57cf8c2964d3b4f2fbda3f02380872835385c5f5
Ali Hüseyn <ali.hsynov@gmail.com>
2025-10-27 19:28:37 +0400

    Created CustomerRepository

---
+++ src/main/java/az/banking/bankmanagementsystem/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package az.banking.bankmanagementsystem.repository;
+
+import az.banking.bankmanagementsystem.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
