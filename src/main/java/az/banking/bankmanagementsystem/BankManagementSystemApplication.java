13ea2d47d4f89645c5c96b9b96c6f623505adfed
Ali Hüseyn <ali.hsynov@gmail.com>
2025-10-27 19:18:46 +0400

    Activating Auditing class

--- src/main/java/az/banking/bankmanagementsystem/BankManagementSystemApplication.java
+++ src/main/java/az/banking/bankmanagementsystem/BankManagementSystemApplication.java
@@ -2,8 +2,10 @@ package az.banking.bankmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BankManagementSystemApplication {
 
     public static void main(String[] args) {
